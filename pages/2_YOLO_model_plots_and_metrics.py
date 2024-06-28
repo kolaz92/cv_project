b52a714e6dd7bf8696b841adfbd6bb4a44a87d4e
Nickolay <kolaz92@gmail.com>
2024-06-28 16:12:55 +0300

    Second page of YOLO model

--- pages/2_YOLO_model_plots_and_metrics.py
+++ pages/2_YOLO_model_plots_and_metrics.py
@@ -1,29 +1,36 @@
 import streamlit as st
 import matplotlib.pyplot as plt    
 import pickle as pkl
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-with open('hist_bird.pkl', 'rb') as file: # словарь оценок для графиков
-        hist_dict = pkl.load(file)
+st.set_page_config(layout="wide")
 
-st.write("# Метрики и графики модели предсказания птиц")
-st.write(f'Accuracy train: {hist_dict["train_accs"][-1]:.4f}, Accuracy valid: {hist_dict["valid_accs"][-1]:.4f}')
+# Путь к изображениям
+image_paths = [
+    'exp3/confusion_matrix.png',  # замените на свои пути к изображениям
+    'exp3/F1_curve.png',
+    'exp3/P_curve.png',
+    'exp3/R_curve.png',
+    'exp3/PR_curve.png'
+]
 
-# зададим функцию рисования графиков
-def plot_history(history, grid=True):
-    fig, ax = plt.subplots(1,2, figsize=(14,5))
+# Создание фигуры и сабплотов
+fig, axes = plt.subplots(5, 1, figsize=(20, 20))  # 1 строка, num_images колонок
 
-    ax[0].plot(history['train_losses'], label='train loss')
-    ax[0].plot(history['valid_losses'], label='valid loss')
-    ax[0].set_title(f'Loss on epoch {len(history["train_losses"])}')
-    ax[0].grid(grid)
-    ax[0].legend()
+axes = axes.flatten()
+# Проход по каждому изображению и его отображение
+for ax, image_path in zip(axes, image_paths):
+    if not image_path:
+        ax.set_axis_off()
+        continue    
+    img = mpimg.imread(image_path)
+    ax.imshow(img)
+    ax.axis('off')  # Отключение осей
 
-    ax[1].plot(history['train_accs'], label='train acc')
-    ax[1].plot(history['valid_accs'], label='valid acc')
-    ax[1].set_title(f'Accuracy on epoch {len(history["train_losses"])}')
-    ax[1].grid(grid)
-    ax[1].legend()
+# Показ фигуры
+plt.tight_layout()
+st.pyplot(fig)
 
-    return fig
-
-st.pyplot(plot_history(hist_dict))
+st.image(mpimg.imread('exp3/results.png'))
+st.image(mpimg.imread('exp3/val_batch2_pred.jpg'))
