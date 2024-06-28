c89b247c8cf664c374125d4f75bcce9625198f4c
Nickolay <kolaz92@gmail.com>
2024-06-28 16:14:43 +0300

    Unmatched f-string crap

--- pages/1_Res_Eff_model_plots_and_metrics.py
+++ pages/1_Res_Eff_model_plots_and_metrics.py
@@ -7,8 +7,8 @@ with open('history.pkl', 'rb') as file: # словарь оценок для г�
 
 st.set_page_config(layout="wide")
 
-st.write("# Метрики и графики модели классификации и предсказания овощей")
-st.write(f'Accuracy train: {log['epoch_train_accuracy'][-1]:.4f}, Accuracy valid: {log['epoch_valid_accuracy'][-1]:.4f}')
+st.write("# Метрики и графики модели классификации и классификации Efficientnet b7")
+st.write(f'Accuracy train: {log["epoch_train_accuracy"][-1]:.4f}, Accuracy valid: {log["epoch_valid_accuracy"][-1]:.4f}')
 
 # зададим функцию рисования графиков
 def plot_history(log, grid=True):
@@ -16,35 +16,37 @@ def plot_history(log, grid=True):
 
     ax = ax.flatten()
 
+    len_ep = len(log['epoch_train_total_loss'])
+
     ax[0].plot(log['epoch_train_total_loss'], label='train loss')
     ax[0].plot(log['epoch_valid_total_loss'], label='valid loss')
-    ax[0].set_title(f'Loss on epoch {len(log['epoch_train_total_loss'])}')
+    ax[0].set_title(f'Loss on epoch {len_ep}')
     ax[0].grid(grid)
     ax[0].legend()
 
 
     ax[1].plot(log['epoch_train_accuracy'], label='train acc')
     ax[1].plot(log['epoch_valid_accuracy'], label='valid acc')
-    ax[1].set_title(f'Accuracy on epoch {len(log['epoch_valid_total_loss'])}')
+    ax[1].set_title(f'Accuracy on epoch {len_ep}')
     ax[1].grid(grid)
     ax[1].legend()
 
     ax[2].plot(log['epoch_train_clf_loss'], label='train')
     ax[2].plot(log['epoch_valid_clf_loss'], label='valid')
-    ax[2].set_title(f'Clf loss on epoch {len(log['epoch_train_total_loss'])}')
+    ax[2].set_title(f'Clf loss on epoch {len_ep}')
     ax[2].grid(grid)
     ax[2].legend()
 
 
     ax[3].plot(log['epoch_train_reg_loss'], label='train')
     ax[3].plot(log['epoch_valid_reg_loss'], label='valid')
-    ax[3].set_title(f'Reg loss on epoch {len(log['epoch_valid_total_loss'])}')
+    ax[3].set_title(f'Reg loss on epoch {len_ep}')
     ax[3].grid(grid)
     ax[3].legend()
 
     ax[4].plot(log['epoch_train_iou'], label='train')
     ax[4].plot(log['epoch_valid_iou'], label='valid')
-    ax[4].set_title(f'IoU on epoch {len(log['epoch_valid_total_loss'])}')
+    ax[4].set_title(f'IoU on epoch {len_ep}')
     ax[4].grid(grid)
     ax[4].legend()
 
